a72d5b747737475207e40dc74332e2869da3c90f
Bethel <brightchinonso951@gmail.com>
2024-08-29 11:42:25 -0700

    second commit

--- src/sections/about/index.jsx
+++ src/sections/about/index.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <div className='about-screen-container'>
       <div className="content-wrapper">
         <p>
-          I am software engineer who specialise in the fields of <strong>fullstack web development</strong> and <strong>web design</strong> , <strong>ui/ux design</strong> and also
+          I am software engineer who specialise in the fields of <strong>fullstack web development</strong>  and <strong>web design</strong> , <strong>ui/ux design</strong> and also
           in <strong>mobile application development</strong>.i dedicate so much attention and time to my job so as to give my clients a very satisfying
           which would enable them to come back looking for more.
         </p>
