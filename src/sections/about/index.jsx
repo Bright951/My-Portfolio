1c869184475745fabfd6ac77f3dae487a41f3f44
Bethel <brightchinonso951@gmail.com>
2024-08-29 07:36:34 -0700

    second commit

--- src/sections/about/index.jsx
+++ src/sections/about/index.jsx
@@ -4,9 +4,17 @@ import './index.scss'
 const About = () => {
   return (
     <div className='about-screen-container'>
-        <div className="content-wrapper">
-          
-        </div>
+      <div className="content-wrapper">
+        <p>
+          I am software engineer who specialise in the fields of <strong>fullstack web development</strong> and <strong>web design</strong> , <strong>ui/ux design</strong> and also
+          in <strong>mobile application development</strong>.i dedicate so much attention and time to my job so as to give my clients a very satisfying
+          which would enable them to come back looking for more.
+        </p>
+        <p>
+          I have been in the tech field with for as long as 3 years and gathered so much experience. i have worked with some companies like
+          Virgycent Stitches and Drippers limited.
+        </p>
+      </div>
     </div>
   )
 }
